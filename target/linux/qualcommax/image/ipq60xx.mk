1c1eec1fae997abe245e1b5e5e0ab1a55b5f901c
breeze303 <grapenskrskr@gmail.com>
2024-10-11 22:51:44 +0800

    qualcommax: ipq60xx: add support for JDCloud AX6600

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -45,6 +45,21 @@ define Device/jdcloud_ax1800-pro
 endef
 TARGET_DEVICES += jdcloud_ax1800-pro
 
+define Device/jdcloud_ax6600
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := JDCloud
+	DEVICE_MODEL := AX6600
+	DEVICE_DTS_CONFIG := config@cp03-c3
+	DEVICE_DTS := ipq6018-jdcloud-ax6600
+	SOC := ipq6018
+	DEVICE_PACKAGES := ipq-wifi-jdcloud_ax6600 kmod-ath11k-pci ath11k-firmware-qcn9074 kmod-fs-ext4 mkf2fs f2fsck kmod-fs-f2fs
+	BLOCKSIZE := 64k
+	KERNEL_SIZE := 6144k
+	IMAGE/factory.bin := append-kernel | pad-to $${KERNEL_SIZE}  |  append-rootfs | append-metadata
+endef
+TARGET_DEVICES += jdcloud_ax6600
+
 define Device/netgear_wax214
        $(call Device/FitImage)
        $(call Device/UbiFit)
